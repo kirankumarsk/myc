3b8fc60e7b6d565a616546309f0848c5da22c0bb
Kiran Kumar S K <57290570+kirankumarsk@users.noreply.github.com>
2021-03-20 20:43:37 +0530

    Add files via upload

---
+++ using while,print hello n times.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+  int count=1,n;
+  /*here we have defined the count which increase as the ++ operator increases as comparable to the value of n*/
+  scanf("%d",&n);
+  while(count<=n)
+   {
+     printf("hello world \n");
+     count++;
+   }
+  return 0;
+}
