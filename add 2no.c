40fc5946b8c3bfaa8f3d547fd890f2327f7db577
Kiran Kumar S K <57290570+kirankumarsk@users.noreply.github.com>
2021-03-20 20:38:26 +0530

    Add files via upload

---
+++ add 2no.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+ main()
+{
+	int a , b, sum;
+	printf("Enter first number:");
+	scanf("%d",&a);
+	 
+	 
+	printf("Enter second number:");
+	scanf("%d",&b);
+	 
+	sum=a+b;
+	printf("The sum of %d and %d is %d",a,b,sum);
+	 
+	 return 0; //Don't forgot this..
+}
